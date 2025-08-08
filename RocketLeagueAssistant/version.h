ed05e51c02d854b357acb2207a358f0eac32ad72
Gtt1229 <gman1229@gmail.com>
2025-08-08 17:13:36 -0400

    Cleanup again..

--- RocketLeagueAssistant/version.h
+++ RocketLeagueAssistant/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 3
 #define VERSION_PATCH 0
-#define VERSION_BUILD 1132
+#define VERSION_BUILD 1133
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
